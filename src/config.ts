1a609aee197cd0ca6ec727852bf607c3b8cef35c
techcodevelop <techcodevelop@gmail.com>
2024-03-25 22:47:37 -0500

    add config.ts 4

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import { config } from "dotenv";
 
 config();
 
-export const PORT: string =process.env.PORT || "3000";
+export const PORT:number = parseInt(process.env.PORT || "3000",10) ;
 export const DB_HOST: string = process.env.DB_HOST || "localhost";
 export const DB_USER: string = process.env.DB_USER || "root";
 export const DB_PASSWORD: string = process.env.DB_PASSWORD || "admin";
 export const DB_DATABASE: string = process.env.DB_DATABASE || "companydb";
-export const DB_PORT: string= process.env.DB_PORT || "3306";
+export const DB_PORT: number= parseInt(process.env.DB_PORT || "3306",10);
